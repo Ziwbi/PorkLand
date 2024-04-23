88d95402486850fd73a86b523bb75af9ee7215a4
Ardent <47233045+Noctice@users.noreply.github.com>
2024-04-24 04:54:48 +0800

    add client control and fix some bugs for ironlord

--- postinit/components/playercontroller.lua
+++ postinit/components/playercontroller.lua
@@ -1,3 +1,4 @@
+local AddComponentPostInit = AddComponentPostInit
 GLOBAL.setfenv(1, GLOBAL)
 local PlayerController = require("components/playercontroller")
 
@@ -19,3 +20,46 @@ local GetPickupAction = function(self, target, tool, ...)
     return _GetPickupAction(self, target, tool, ...)
 end
 ToolUtil.SetUpvalue(PlayerController.GetActionButtonAction, GetPickupAction, "GetPickupAction")
+
+function PlayerController:ReleaseControlSecondary(x, z)
+    if not self.ismastersim then
+        SendModRPCToServer(MOD_RPC["Porkland"]["ReleaseControlSecondary"], x, z)
+    end
+    local position = Vector3(x, 0, z)
+    if self.inst.sg ~= nil and self.inst.sg:HasStateTag("strafing") then
+        self.inst:PushBufferedAction(BufferedAction(self.inst, nil, ACTIONS.CHARGE_RELEASE, nil, position))
+    end
+end
+
+function PlayerController:OnRemoteReleaseControlSecondary(x, z)
+    local position = Vector3(x, 0, z)
+    if self.inst.sg ~= nil and self.inst.sg:HasStateTag("strafing") then
+        self.inst:PushBufferedAction(BufferedAction(self.inst, nil, ACTIONS.CHARGE_RELEASE, nil, position))
+    end
+end
+
+local _OnUpdate = PlayerController.OnUpdate
+function PlayerController:OnUpdate(dt)
+    local ret = {_OnUpdate(self, dt)}
+
+    local isenabled, ishudblocking = self:IsEnabled()
+
+    if self.handler then
+        if self.lasttick_controlpressed[CONTROL_SECONDARY] ~= nil
+            and self.lasttick_controlpressed[CONTROL_SECONDARY] == true
+            and self:IsControlPressed(CONTROL_SECONDARY) == false then
+            local x, z = TheInput:GetWorldXZWithHeight(1)
+            self:ReleaseControlSecondary(x, z)
+        end
+        self.lasttick_controlpressed[CONTROL_SECONDARY] = self:IsControlPressed(CONTROL_SECONDARY)
+    end
+
+    return unpack(ret)
+end
+
+AddComponentPostInit("playercontroller", function(self)
+    self.vulnerabletopoisondamage = true
+    self.poison_damage_scale = 1
+
+    self.lasttick_controlpressed = {}
+end)
--- postinit/stategraphs/SGwilson_client.lua
+++ postinit/stategraphs/SGwilson_client.lua
@@ -20,6 +20,13 @@ local actionhandlers = {
     ActionHandler(ACTIONS.SHEAR, function(inst)
         return not inst.sg:HasStateTag("preshear") and "shear_start" or nil
     end),
+    ActionHandler(ACTIONS.CHARGE_UP, "ironlord_charge"),
+    ActionHandler(ACTIONS.CHARGE_RELEASE, function(inst, action)
+        if inst.sg:HasStateTag("strafing") then
+            inst.sg.statemem.should_shoot = true
+            inst.sg.mem.shootpos = action:GetActionPoint()
+        end
+    end),
 }
 
 local states = {
@@ -161,6 +168,109 @@ local states = {
             inst:ClearBufferedAction()
             inst.sg:GoToState("idle")
         end,
+    },
+
+    State{
+        name = "ironlord_idle",
+        tags = {"idle", "canrotate"},
+
+        onenter = function(inst, pushanim)
+            inst.components.locomotor:StopMoving()
+
+            if pushanim then
+                inst.AnimState:PlayAnimation(pushanim)
+                inst.AnimState:PushAnimation("idle_loop", true)
+            else
+                inst.AnimState:PlayAnimation("idle_loop", true)
+            end
+
+        end,
+
+       events =
+        {
+            EventHandler("animover", function(inst)
+                inst.sg:GoToState("ironlord_idle")
+            end),
+        },
+    },
+
+    State{
+        name = "ironlord_charge",
+        tags = {"busy", "doing", "strafing"},
+        server_states = {"ironlord_charge", "ironlord_charge_full"},
+
+        onenter = function(inst)
+            inst.components.locomotor:Stop()
+            inst.AnimState:PlayAnimation("charge_pre")
+            inst.AnimState:PushAnimation("charge_grow")
+            inst.SoundEmitter:PlaySound("dontstarve_DLC003/common/crafted/iron_lord/charge_up_LP", "chargedup")
+
+            inst.sg.statemem.ready_to_shoot = false
+            inst.sg.statemem.should_shoot = false
+
+            inst:PerformPreviewBufferedAction()
+        end,
+
+        onexit = function(inst)
+            inst.SoundEmitter:KillSound("chargedup")
+        end,
+
+        onupdate = function(inst)
+            if inst.sg.statemem.should_shoot and inst.sg.statemem.ready_to_shoot then
+                if inst.sg.statemem.isfull then
+                    inst.SoundEmitter:PlaySoundWithParams("dontstarve_DLC003/creatures/boss/hulk_metal_robot/laser",  {intensity = math.random(0.7, 1)})
+                else
+                    inst.SoundEmitter:PlaySoundWithParams("dontstarve_DLC003/common/crafted/iron_lord/smallshot", {timeoffset = math.random()})
+                end
+                inst.SoundEmitter:KillSound("chargedup")
+                inst.sg:GoToState("ironlord_shoot", false)
+            end
+        end,
+
+        timeline =
+        {
+            TimeEvent(15 * FRAMES, function(inst) inst.sg.statemem.ready_to_shoot = true end),
+            TimeEvent(20 * FRAMES, function(inst)
+                inst.AnimState:PlayAnimation("charge_super_pre")
+                inst.AnimState:PushAnimation("charge_super_loop", true)
+                inst.SoundEmitter:PlaySound("dontstarve_DLC003/common/crafted/iron_lord/electro")
+                inst.sg.statemem.ready_to_shoot = false
+                inst.sg.statemem.should_shoot = false
+            end),
+            TimeEvent(25 * FRAMES, function(inst) inst.sg.statemem.ready_to_shoot = true end),
+        },
+    },
+
+
+    State{
+        name = "ironlord_shoot",
+        tags = {"busy"},
+        server_states = {"ironlord_shoot"},
+
+        onenter = function(inst, is_full_charge)
+            inst.components.locomotor:Stop()
+            if is_full_charge then
+                inst.AnimState:PlayAnimation("charge_super_pst")
+            else
+                inst.AnimState:PlayAnimation("charge_pst")
+            end
+            inst.sg.statemem.is_full_charge = is_full_charge
+
+            if inst.sg.mem.shootpos ~= nil then
+                inst:ForceFacePoint(inst.sg.mem.shootpos:Get())
+            end
+        end,
+
+        timeline =
+        {
+            TimeEvent(5 * FRAMES, function(inst) inst.sg:RemoveStateTag("busy") end),
+
+        },
+
+        events =
+        {
+            EventHandler("animover", function(inst) inst.sg:GoToState("ironlord_idle") end),
+        },
     },
 }
 
